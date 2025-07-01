cba107774370251884f6ed02ba9cbc6f4ada696a
SHUBHAM GARG <gargshubh1107@gmail.com>
2025-07-01 13:03:27 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    // Get matrix dimensions from user
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix1[rows][cols], matrix2[rows][cols], sum[rows][cols];
+
+    // Input first matrix
+    printf("Enter elements of Matrix 1:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter elements of Matrix 2:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    // Calculate sum of matrices
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    // Display result
+    printf("Sum of the matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
